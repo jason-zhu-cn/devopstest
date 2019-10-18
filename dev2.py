8ac4b7231f444a08b563ebb86b23e2cbc2c942d3
laozhu <laozhu@tt.com>
2019-10-18 13:36:51 +0800

    modified dev2.py

--- dev2.py
+++
@@ -1,9 +0,0 @@
-
-#
-# /etc/fstab
-# Created by anaconda on Thu Jul 11 02:52:01 2019
-#
-# Accessible filesystems, by reference, are maintained under '/dev/disk'
-# See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
-#
-UUID=1114fe9e-2309-4580-b183-d778e6d97397 /                       ext4    defaults        1 1
